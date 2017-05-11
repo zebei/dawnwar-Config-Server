0540ecb954b55518c1c3e6b57e8c7e4355ee37e0
lizebei <lizebei@LIZEBEI.ds.gome.com.cn>
2017-05-11 14:56:26 +0800

    create config server

---
+++ src/main/java/com/didispace/Application.java
@@ -0,0 +1,22 @@
+package com.didispace;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		long starTime = System.currentTimeMillis();
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+		long endTime = System.currentTimeMillis();
+		long time = endTime - starTime;
+		System.out.println("\nStart Time: " + time / 1000 + " s");
+		System.out.println("...............................................................");
+		System.out.println("..............Config Service starts successfully...............");
+		System.out.println("...............................................................");
+	}
+
+}
