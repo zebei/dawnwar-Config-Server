68bb5a45456ec1044e9f8dd026b0e2766b932e0c
lizebei <lizebei@LIZEBEI.ds.gome.com.cn>
2017-05-11 16:38:31 +0800

    rename package

--- src/main/java/com/dawnwar/ConfigApplication.java
+++ src/main/java/com/dawnwar/ConfigApplication.java
@@ -1,10 +1,12 @@
-package com.didispace;
+package com.dawnwar;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigApplication {
 
